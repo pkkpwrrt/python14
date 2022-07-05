56e9c931ca141459c0aeb8fe7ba65b5cc11c53e0
pkkpwrrt <47749@cru.ac.th>
2022-07-05 14:21:58 +0700

    first commit

---
+++ profile.py
@@ -0,0 +1,10 @@
+profile = {
+    "name" : "Pakkapon",
+    "SurName" : "Worarat",
+    "nickname" : "Satang",
+    "age" : 17,
+    "hobbies" : ["play a game","Basketball","Swimming"],
+    "food" : {"ข้าวหลามยายชิ้น":"Nongmon city","Pizza":"The pizza company","Steak": "Lung Yhik","Ramen":"Hachiban Ramen","Khao Pad Krapraw":"Jae So"},
+}
+print(profile)
+#นาย ภัคพล วรรัตน์ 6/14 เลขที่ 31
